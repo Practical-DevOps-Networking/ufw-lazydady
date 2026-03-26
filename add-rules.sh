e5736f533be401532c2d76b3597f32e27c917953
lazydady <ihorshamshur@gmail.com>
2026-03-26 13:08:43 +0200

    Update firewall rules in add-rules.sh

--- add-rules.sh
+++ add-rules.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+ ufw allow from 172.168.0.100 to any port 3306
+ ufw allow from 192.168.32.55 to any port 3005
+ ufw reject to any port 3005
+ ufw allow in on eth0 to any port 8099
+ ufw limit 6050:6055/tcp
+ ufw enable
